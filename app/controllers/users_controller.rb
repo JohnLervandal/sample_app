4973876d573950fb51e00c9bb5c9f107f8df5771
JohnLervandal <JohnLervandal@brandeis.edu>
2022-10-06 12:29:50 -0400

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,22 +9,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # Not the final implementation!
-    if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      # Handle a successful save.
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
-
-  def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      reset_session
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
-      render 'new'
+      render 'new', status: :unprocessable_entity
     end
   end
 
@@ -34,5 +26,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-
 end
